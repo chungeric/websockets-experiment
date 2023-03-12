e66acc7b41f39286c24b067114161a7d7a0d91be
Eric Chung <rchung.eric@gmail.com>
2023-03-12 18:03:29 +1100

    Add express, socket.io & use Player class

--- Player.js
+++ Player.js
@@ -1,34 +1,54 @@
-export default class Player {
-  constructor(x, y, color, lifetime) {
+function rr(min, max) {
+  return Math.floor(Math.random() * (max - min) + min)
+}
+
+function generateRandomColor() {
+  const h = Math.round(Math.random() * 255);
+  const s = rr(80, 95);
+  const l = 65;
+  return `${h}, ${s}%, ${l}%`;
+}
+
+class Player {
+  constructor() {
     this.x = 0;
     this.y = 0;
-    this.color = `220, 80, 60`; // hsl
+    this.score = 0;
+    this.color = generateRandomColor(); // hsl
     this.trail = []; // [{ x, y, age }]
-    this.lifetime = 30; // 30 frames
   }
   update(x, y) {
     this.trail.forEach((trailItem, i) => {
-      this.trail[i] = { ...trailItem, age: age + 1 };
+      this.trail[i] = { ...trailItem, age: trailItem.age + 1 };
       if (trailItem.age >= 30) this.trail.splice(i, 1);
     })
-    this.trail.push(this.x, this.y);
-    this.x = x;
-    this.y = y;
+    this.trail.push({ x: this.x, y: this.y, age: 0 });
+    if (x) {
+      this.x = x;
+    }
+    if (y) {
+      this.y = y;
+    }
   }
-  drawCircle(ctx, x, y, alpha) {
-    ctx.globalAlpha = alpha;
-    ctx.fillStyle = 'black';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    const xx = ctx.canvas.clientWidth * x;
-    const yy = ctx.canvas.clientHeight * y;
-    ctx.beginPath();
-    ctx.arc(xx, yy, 100, 0, 2 * Math.PI);
-    ctx.fillStyle = `hsl(${this.color})`;
-    ctx.fill();
-    ctx.globalAlpha = 1;
+  addToScore(pointsToAdd = 1) {
+    this.score += pointsToAdd;
   }
-  draw(ctx) {
-    this.drawCircle(ctx, this.x, this.y, 1);
-    this.trail.forEach(trailItem => this.drawCircle(ctx, trailItem.x, trailItem.y, 1 - trailItem.age / this.lifetime));
-  }
+  // drawCircle(ctx, x, y, alpha) {
+  //   ctx.globalAlpha = alpha;
+  //   ctx.fillStyle = 'black';
+  //   ctx.fillRect(0, 0, canvas.width, canvas.height);
+  //   const xx = ctx.canvas.clientWidth * x;
+  //   const yy = ctx.canvas.clientHeight * y;
+  //   ctx.beginPath();
+  //   ctx.arc(xx, yy, 100, 0, 2 * Math.PI);
+  //   ctx.fillStyle = `hsl(${this.color})`;
+  //   ctx.fill();
+  //   ctx.globalAlpha = 1;
+  // }
+  // draw(ctx) {
+  //   this.drawCircle(ctx, this.x, this.y, 1);
+  //   this.trail.forEach(trailItem => this.drawCircle(ctx, trailItem.x, trailItem.y, 1 - trailItem.age / this.lifetime));
+  // }
 }
+
+module.exports = Player;
---
+++ server.js
@@ -0,0 +1,57 @@
+const { Server } = require("socket.io");
+const express = require('express');
+const http = require('http');
+const app = express();
+const server = http.createServer(app);
+const port = 3000;
+const io = new Server(server);
+const Player = require('./Player');
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+})
+
+server.listen(port, '0.0.0.0');
+
+// Player data store { <websocket_id>: <data> }
+// const playersData = {};
+const store = {
+  playersData: {},
+  foodPos: { x: Math.random(), y: Math.random() },
+};
+
+io.on('connection', (socket) => {
+  console.log('connected', socket.id);
+  store.playersData[socket.id] = new Player();
+  io.emit('updated-data', store.playersData);
+  io.emit('update-scoreboard');
+  io.emit('food-spawned', store.foodPos);
+  
+  // update loop
+  function update() {
+    store.playersData[socket.id]?.update();
+    io.emit('updated-data', store.playersData);
+  }
+  setInterval(() => update(), 32);
+
+  // socket event listeners
+  socket.on('move', ({ x: newX, y: newY }) => {
+    store.playersData[socket.id].update(newX, newY);
+    io.emit('updated-data', store.playersData);
+  });
+  socket.on('food-eaten', () => {
+    store.playersData[socket.id].addToScore();
+    store.foodPos = { x: Math.random(), y: Math.random() };
+    io.emit('updated-data', store.playersData);
+    io.emit('update-scoreboard');
+    io.emit('food-spawned', store.foodPos);
+  })
+  socket.on("disconnect", () => {
+    delete store.playersData[socket.id];
+    console.log('disconnected', socket.id);
+    io.emit('updated-data', store.playersData);
+    io.emit('update-scoreboard');
+  });
+});
